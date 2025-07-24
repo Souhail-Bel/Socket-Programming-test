f86172dc2a441f3e616b6da17b118795ba22aae1
Souhail-Bel <robotboy976@gmail.com>
2025-07-24 23:23:12 +0100

    Added clock GUI

---
+++ client_time_GUI.py
@@ -0,0 +1,80 @@
+import sys
+import socket
+import traceback
+# import struct
+import time
+from time import strftime, localtime
+
+import tkinter as tk
+
+# Function to update the digital clock label
+def tick():
+    str_NIST_t = strftime(DATE_FORMAT, localtime(t - dt))
+    str_SYS_t  = strftime(DATE_FORMAT, localtime(int(time.time())))
+    NIST_clock.config(text=str_NIST_t)
+    SYS_clock.config(text=str_SYS_t)
+    NIST_clock.after(1000, tick)
+
+
+host = "time.nist.gov"
+port = 37
+
+# The time server returns number of seconds since 1900-01-01 while the Unix system has it since 1970-01-01, we just ought to account for that difference
+
+# Time protocol, RFC 868
+dt = 2208988800
+
+DATE_FORMAT = "%Y-%m-%d, %H:%M:%S"
+
+
+if __name__ == "__main__":
+    
+    try:
+        with socket.socket() as s:
+            s.connect((host, port))
+            print("Connection established.")
+            
+            res = b""
+            
+            while True:
+                buff = s.recv(4096)
+                if not buff:
+                    break
+                res += buff
+                
+            # !: Network byte order, I: unsigned
+            # t = struct.unpack('!I', res)[0]
+            # t = 3961996701 # For when the server fails
+            t = int.from_bytes(res, "big")
+            t_sys = int(time.time()) + dt
+            
+            print("===== TIME SINCE 1900-01-01 =====")
+            print("NIST :", t, " seconds")
+            print("SYS  :", t_sys, " seconds")
+            
+            print()
+            
+            print("=========== FORMATTED ===========")
+            print("NIST :", strftime(DATE_FORMAT, localtime(t - dt)))
+            print("SYS  :", strftime(DATE_FORMAT, localtime(t_sys - dt)))
+            
+            root = tk.Tk()
+            root.title("C L O C K")
+
+            # NIST Clock
+            tk.Label(root, font=('monospace', 40, 'bold'), text="NIST:").pack(pady=(20, 0))
+            NIST_clock = tk.Label(root, font=('monospace', 40, 'bold'))
+            NIST_clock.pack(pady=(0, 20))
+
+            # SYS Clock
+            tk.Label(root, font=('monospace', 40, 'bold'), text="SYS:").pack(pady=(20, 0))
+            SYS_clock = tk.Label(root, font=('monospace', 40, 'bold'))
+            SYS_clock.pack(pady=(0, 20))
+
+
+            tick()
+            root.mainloop()
+            
+    except:
+        print(traceback.format_exc())
+    
